d08cdbf2cdbee63f5bcb4a5487ac4e074e6a2947
Davehow <david.howard1100@gmail.com>
2023-01-18 12:08:18 -0500

    cleanup

--- src/Howbot.Infrastructure/Data/AppDbContext.cs
+++ src/Howbot.Infrastructure/Data/AppDbContext.cs
@@ -9,9 +9,7 @@ public class AppDbContext : DbContext
   public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
   {
   }
-
-  public DbSet<UrlStatusHistory> UrlStatusHistories { get; set; }
-
+  
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
     base.OnModelCreating(modelBuilder);
