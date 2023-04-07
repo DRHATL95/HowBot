f6f288c7e216326ccd27bca5430f261d5eca01c8
David Howard <david.howard1100@gmail.com>
2023-04-07 13:39:10 -0400

    update version of .net sdk & create scripts/production.sh & change env Youtube -> YoutubeToken

---
+++ scripts/production.sh
@@ -0,0 +1,53 @@
+﻿#!/bin/bash
+
+set -e
+
+if command -v docker &> /dev/null && command -v dotnet &> /dev/null; then
+    echo "Docker and .NET are installed!"
+    
+    LAVANODE_STATUS="$(docker ps -q -f name=lavanode)"
+    
+    if [ -z "$LAVANODE_STATUS" ]; then
+        echo "Starting lavalink docker container..."
+        docker run -d -p 2333:2333 --name lavanode fredboat/lavalink:latest
+    else
+        echo "Lava Node is already running! No need to run."
+    fi  
+    
+    # Define environment variables
+    DiscordTokenProd=$(DiscordTokenProd)
+    DiscordLavalinkServerPassword=$(DiscordLavalinkServerPassword)
+    YoutubeToken=$(Youtube)
+    
+    # Export environment variables separately to avoid masking the return values
+    export DiscordTokenProd
+    export DiscordLavalinkServerPassword
+    export YoutubeToken
+    
+    # Change to project directory
+    cd "$(DeploymentPath)"
+    
+    # Give the worker executable permissions
+    chmod 777 ./Howbot.Worker
+    
+    # Run the worker using nohup to prevent the process from being killed when the SSH session ends
+    nohup ./Howbot.Worker > ~/Code/Production/HowBot/Howbot.Worker.log 2>&1 &
+    
+    echo "Howbot.Worker is now running!"
+else
+    # Check if Docker is installed
+    if command -v docker &> /dev/null
+    then
+        echo "Docker is installed"
+    else
+        echo "Docker is not installed"
+    fi
+    
+    # Check if .NET is installed
+    if command -v dotnet &> /dev/null
+    then
+        echo ".NET is installed"
+    else
+        echo ".NET is not installed"
+    fi
+fi
