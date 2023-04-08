547ca6f48e8371cd5c717d7f9a9b6f8c6f2798a7
David Howard <david.howard1100@gmail.com>
2023-04-08 17:28:29 -0400

    update production.sh

--- scripts/production.sh
+++ scripts/production.sh
@@ -3,32 +3,54 @@
 if command -v docker &> /dev/null && command -v dotnet &> /dev/null; then
     echo "Docker and .NET are installed!"
     
-    LAVANODE_STATUS="$(docker ps -q -f name=lavanode)"
+    if [ -z "${DiscordTokenProd}" ]; then
+        echo "DiscordTokenProd is not set. Setting now.."
+        
+        # Set the DiscordTokenProd environment variable to the value in the release pipelines variable group
+        DiscordTokenProd=$(DiscordTokenProd)
+        export DiscordTokenProd
+    fi
     
-    if [ -z "$LAVANODE_STATUS" ]; then
+    if [ -z "${DiscordLavalinkServerPassword}" ]; then
+        echo "DiscordLavalinkServerPassword is not set. Setting now.."
+        
+        # Set the DiscordLavalinkServerPassword environment variable to the value in the release pipelines variable group
+        DiscordLavalinkServerPassword=$(DiscordLavalinkServerPassword)
+        export DiscordLavalinkServerPassword
+    fi
+    
+    if [ -z "${YoutubeToken}" ]; then
+        echo "YoutubeToken is not set. Setting now.."
+        
+        # Set the YoutubeToken environment variable to the value in the release pipelines variable group
+        YoutubeToken=$(Youtube)
+        export YoutubeToken
+    fi
+        
+    # Check if the Lava Node is running
+    if [ "$(docker ps -q -f name=lavanode)" ]; then
         echo "Starting lavalink docker container..."
+        # Start the Lava Node container on port 2333
         docker run -d -p 2333:2333 --name lavanode fredboat/lavalink:latest
+        # Copy the backups/application.yml.example file to the container
         docker cp ~/Code/backups/application.yml.example lavanode:/opt/Lavalink/application.yml
+        # Restart the container to apply the changes
         docker container restart lavanode
     else
         echo "Lava Node is already running! No need to run."
-    fi  
+    fi    
     
-    # Define environment variables
-    DiscordTokenProd=$(DiscordTokenProd)
-    DiscordLavalinkServerPassword=$(DiscordLavalinkServerPassword)
-    YoutubeToken=$(Youtube)
-    
-    # Export environment variables separately to avoid masking the return values
-    export DiscordTokenProd
-    export DiscordLavalinkServerPassword
-    export YoutubeToken
+    echo "Changing to project directory..."
     
     # Change to project directory
     cd Code/Production/HowBot || exit
     
+    echo "Giving Howbot.Worker executable permissions..."
+    
     # Give the worker executable permissions
     chmod 777 ./Howbot.Worker || exit
+    
+    echo "Starting Howbot.Worker..."
     
     # Run the worker using nohup to prevent the process from being killed when the SSH session ends
     nohup ./Howbot.Worker > ~/Code/Production/HowBot/Howbot.Worker.log 2>&1 &
