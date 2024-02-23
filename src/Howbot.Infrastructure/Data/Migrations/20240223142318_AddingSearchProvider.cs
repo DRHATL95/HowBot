98ac109ff143437c4fe45c731db3ea265a3049f4
David Howard <david.howard1100@gmail.com>
2024-02-23 09:33:48 -0500

    Adding migration for previous commit

---
+++ src/Howbot.Infrastructure/Data/Migrations/20240223142318_AddingSearchProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Howbot.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingSearchProvider : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SearchProvider",
+                table: "Guilds",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SearchProvider",
+                table: "Guilds");
+        }
+    }
+}
